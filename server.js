7ccef13769804f42f9c6563c85101dfa750a5ab8
Benjie Velarde <bvelarde@gmail.com>
2021-12-25 10:59:24 +0800

    feat: all api calls proxied

--- server.js
+++ server.js
@@ -24,19 +24,93 @@ app.get('/', (_, res) => {
   res.sendFile(process.cwd() + '/ui/build/index.html');
 });
 
+const getHeaders = (domain = 'CDH') => {
+  return {
+    'Content-Type': 'application/json',
+    'cp-site-domain': domain
+  }
+};
+
 const http = axios.create({
   baseURL: process.env.SHIP_MANAGEMENT_API_URL,
-  headers: {
-    'Content-Type': 'application/json'
-  },
   timeout: process.env.REQUEST_TIMEOUT
 });
 
-app.get('/vessels', async (_, res) => {
-  const { data } = await http.get(`/Vessels?organizationId=${process.env.ORG_ID}`, {
-    headers: {
-      'cp-site-domain': 'CDH'
-    }
+app.get('/api/vessels/:orgId', async (req, res) => {
+  const { orgId } = req.params;
+  const { data } = await http.get(`/Vessels?organizationId=${orgId}`, {
+    headers: getHeaders()
+  });
+
+  res.status(200).send(data);
+});
+
+app.get('/api/documents/:aeCode', async (req, res) => {
+  const { aeCode } = req.params;
+  const { data } = await http.get(`/Vessels/Vessel%20Documents/${aeCode}/Documents/files`, {
+    headers: getHeaders()
+  });
+
+  res.status(200).send(data);
+});
+
+app.get('/api/financial/vessel/:aeCode/:vessel', async (req, res) => {
+  const { aeCode, vessel } = req.params;
+  const { data } = await http.get(`/Vessels/Client%20Financial%20Reports/${aeCode}/${vessel}/files`, {
+    headers: getHeaders()
+  });
+
+  res.status(200).send(data);
+});
+
+app.get('/api/financial/fleet/:orgId', async (req, res) => {
+  const { orgId } = req.params;
+  const { data } = await http.get(`/Vessels/Client%20Financial%20Reports/ZZZ${orgId}/ZZ_REPORTS%20MULTIPLE%20SHIPS/files`, {
+    headers: getHeaders()
+  });
+
+  res.status(200).send(data);
+});
+
+app.get('/api/reports/:aeCode/:category', async (req, res) => {
+  const { category, aeCode } = req.params;
+  const { data } = await http.get(`/Vessels/Vessel%20Documents/${aeCode}/${category}/files`, {
+    headers: getHeaders()
+  });
+
+  res.status(200).send(data);
+});
+
+app.get('/api/leadership', async (req, res) => {
+  const { data } = await http.get(`/Resources/leadership`, {
+    headers: getHeaders()
+  });
+
+  res.status(200).send(data);
+});
+
+app.get('/api/papers/:category', async (req, res) => {
+  const { category } = req.params;
+  const { data } = await http.get(`/Resources/${category}/files`, {
+    headers: getHeaders()
+  });
+
+  res.status(200).send(data);
+});
+
+app.get('/api/download-url/:driveName/:fileId', async (req, res) => {
+  const { driveName, fileId } = req.params;
+  const { data } = await http.get(`/Files/${driveName}/${fileId}`, {
+    headers: getHeaders()
+  });
+
+  res.status(200).send(data);
+});
+
+app.get('/api/preview-url/:driveName/:fileId', async (req, res) => {
+  const { driveName, fileId } = req.params;
+  const { data } = await http.get(`/Files/${driveName}/${fileId}`, {
+    headers: getHeaders()
   });
 
   res.status(200).send(data);
