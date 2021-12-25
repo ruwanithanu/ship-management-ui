7c45d84f1c406055ebf522c938970fcbbc6d0c71
Benjie Velarde <bvelarde@gmail.com>
2021-12-25 12:23:26 +0800

    refactor: revert proxied api calls

--- ui/src/api/http.ts
+++ ui/src/api/http.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const {
-  REACT_APP_SVC_URL: baseURL,
+  REACT_APP_SHIP_MANAGEMENT_API_URL: baseURL,
   REACT_APP_REQUEST_TIMEOUT: timeout
 } = process.env;
 
@@ -12,5 +12,24 @@ const http = axios.create({
   },
   timeout: timeout ? parseInt(timeout, 10) : 180000
 });
+
+type SP_Domain = 'CDH' | 'AETEC';
+type Auth_Bearer = string | undefined;
+
+interface ExtraHeaders {
+  bearer?: Auth_Bearer;
+  domain?: SP_Domain;
+};
+
+export const extraHeaders = ({bearer, domain = 'CDH'}: ExtraHeaders) => {
+  const headers: any = {
+    'Content-Type': 'application/json',
+    'cp-site-domain': domain
+  };
+  if (bearer) {
+    headers['Authorization'] = `BEARER ${bearer}`;
+  }
+  return headers;
+}
 
 export default http;
--- ui/src/api/index.ts
+++ ui/src/api/index.ts
@@ -1,5 +1,6 @@
 import { memoize } from '@/utils';
-import http from './http';
+import http, { extraHeaders } from './http';
+import axios from 'axios';
 import { Vessel } from '@/types';
 
 import { Downloadable } from '@/types';
@@ -7,42 +8,68 @@ import { Downloadable } from '@/types';
 const { REACT_APP_ORG_ID: ORG_ID } = process.env;
 
 export const fetchVessels = memoize(async () => {
-  const { data } = await http.get(`/vessels/${ORG_ID}`);
+  // const headers = extraHeaders({});
+  // const { data } = await http.get(`/Vessels?organizationId=${ORG_ID}`, {
+  //   headers
+  // });
+  // return data;
+  const { data } = await axios.get('/vessels');
   return data;
 });
 
 export const fetchDocuments = memoize(async (aeCode: string) => {
-  const { data } = await http.get(`/documents/${aeCode}`);
+  const headers = extraHeaders({});
+  const { data } = await http.get(`/Vessels/Vessel%20Documents/${aeCode}/Documents/files`, {
+    headers
+  });
   return data;
 });
 
 export const fetchFinancialVesselReports = memoize(async (vessel: Vessel) => {
-  const { data } = await http.get(`/financial/vessel/${vessel.aeCode}/${vessel.name}`);
+  const headers = extraHeaders({});
+  const { data } = await http.get(`/Vessels/Client%20Financial%20Reports/${vessel.aeCode}/${vessel.name}/files`, {
+    headers
+  });
   return data;
 });
 
 export const fetchFinancialFleetReports = memoize(async () => {
-  const { data } = await http.get(`/financial/fleet/${ORG_ID}`);
+  const headers = extraHeaders({});
+  const { data } = await http.get(`/Vessels/Client%20Financial%20Reports/ZZZ${ORG_ID}/ZZ_REPORTS%20MULTIPLE%20SHIPS/files`, {
+    headers
+  });
   return data;
 });
 
 export const fetchReportsClaims = memoize(async (aeCode: string) => {
-  const { data } = await http.get(`/reports/${aeCode}/Claims`);
+  const headers = extraHeaders({});
+  const { data } = await http.get(`/Vessels/Vessel%20Documents/${aeCode}/Claims/files`, {
+    headers
+  });
   return data;
 });
 
 export const fetchReportsCommercial = memoize(async (aeCode: string) => {
-  const { data } = await http.get(`/reports/${aeCode}/Commercial`);
+  const headers = extraHeaders({});
+  const { data } = await http.get(`/Vessels/Vessel%20Documents/${aeCode}/Commercial/files`, {
+    headers
+  });
   return data;
 });
 
 export const fetchReportsGallery = memoize(async (aeCode: string) => {
-  const { data } = await http.get(`/reports/${aeCode}/Gallery`);
+  const headers = extraHeaders({});
+  const { data } = await http.get(`/Vessels/Vessel%20Documents/${aeCode}/Gallery/files`, {
+    headers
+  });
   return data;
 });
 
 export const fetchReportsOther = memoize(async (aeCode: string) => {
-  const { data } = await http.get(`/reports/${aeCode}/Other%20Reports`);
+  const headers = extraHeaders({});
+  const { data } = await http.get(`/Vessels/Vessel%20Documents/${aeCode}/Other%20Reports/files`, {
+    headers
+  });
   return data;
 });
 
@@ -57,31 +84,51 @@ export const fetchDrawings = memoize(async () => {
 });
 
 export const fetchLeadership = memoize(async () => {
-  const { data } = await http.get(`/leadership`);
+  const headers = extraHeaders({});
+  const { data } = await http.get(`/Resources/leadership`, {
+    headers
+  });
   return data;
 });
 
 export const fetchLookouts = memoize(async () => {
-  const { data } = await http.get(`/papers/Lookout`);
+  const headers = extraHeaders({});
+  const { data } = await http.get(`/Resources/Lookout/files`, {
+    headers
+  });
   return data;
 });
 
 export const fetchPSC = memoize(async () => {
-  const { data } = await http.get(`/papers/PSC`);
+  const headers = extraHeaders({});
+  const { data } = await http.get(`/Resources/PSC/files`, {
+    headers
+  });
   return data;
 });
 
 export const fetchTechnicalPapers = memoize(async () => {
-  const { data } = await http.get(`/papers/Technical%20Papers`);
+  const headers = extraHeaders({});
+  const { data } = await http.get(`/Resources/Technical%20Papers/files`, {
+    headers
+  });
   return data;
 });
 
 export const getDownloadURL = memoize(async (file: Downloadable) => {
-  const { data } = await http.get(`/download-url/${file.driveName}/${file.id}`);
-  return data;
+  if (file.hasOwnProperty('driveName')) {
+    const { data: { url } } = await http.get(`/Files/${file.driveName}/${file.id}`);
+    return url;
+  } else {
+    return '/assets/sample.pdf';
+  }
 });
 
 export const getPreviewURL = memoize(async (file: Downloadable) => {
-  const { data } = await http.get(`/preview-url/${file.driveName}/${file.id}`);
-  return data;
+  if (file.hasOwnProperty('driveName')) {
+    const { data: { url } } = await http.get(`/Files/preview/${file.driveName}/${file.id}`);
+    return url;
+  } else {
+    return '/assets/sample.pdf';
+  }
 });
